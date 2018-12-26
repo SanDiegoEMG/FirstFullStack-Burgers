fc21d3c5df8b79b8c75750ba8c72117d9ebe7aa0
SD_emg <sandiegoemg@gmail.com>
2018-12-26 11:56:35 -0800

    accepting json and form data in postman

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+// tells server to accept these forms of data
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
     res.send("One step at a time")
+});
+
+app.post("/api/new", function(req, res){
+    res.json(req.body);
 });
 
 app.listen(PORT, function(){
