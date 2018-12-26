5c6b16413d10628bc5b1b12ad8de07c15014e6d2
SD_emg <sandiegoemg@gmail.com>
2018-12-26 12:41:55 -0800

    returning home.handlebars via express route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require(`dotenv`).config();
 var express = require("express");
 var app = express();
 var mysql = require('mysql');
+var exphbs  = require('express-handlebars');
+
 
 var PORT = process.env.PORT || 8080;
 
@@ -26,8 +28,16 @@ connection.connect(function (err) {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+// handlebar requirements -- must be above the routes
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get("/", function (req, res) {
     res.send("Fight one giant at a time")
+});
+
+app.get("/handle", function (req, res) {
+    res.render("home");
 });
 
 app.get("/table", function (req, res){
